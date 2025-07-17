c7fe44e368aee1ec8e29845a5e16789a6ce2313c
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-17 13:32:07 +0530

    initial command

--- index.js
+++ index.js
@@ -1,51 +1,93 @@
 // 1.question
-let num = 5;
+// let num = 5;
 
-if (num > 0) {
-  console.log("Positive number");
-} else if (num === 0) {
-  console.log("Zero");
+// if (num > 0) {
+//   console.log("Positive number");
+// } else if (num === 0) {
+//   console.log("Zero");
+// } else {
+//   console.log("Negative number");
+// }
+// // 2.question
+// let number = 7;
+
+// if (number % 2 === 0) {
+//   console.log("Even number");
+// } else {
+//   console.log("Odd number");
+// }
+// // 3.question
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not eligible to vote");
+// }
+// // 4.question
+// let marks = 82;
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 75) {
+//   console.log("Grade B");
+// } else if (marks >= 50) {
+//   console.log("Grade C");
+// } else {
+//   console.log("Grade F");
+// }
+// // 5.question
+// let year = 2024;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//   console.log("Leap year");
+// } else {
+//   console.log("Not a leap year");
+// }
+// // 6.question
+// let x = -3;
+
+// let result = (x < 0) ? "Negative number" : "Not negative";
+// console.log(result);
+
+// // 7.question
+// let char = 'e';
+// char = char.toLowerCase();
+
+// if ('aeiou'.includes(char)) {
+//   console.log("Vowel");
+// } else {
+//   console.log("Consonant");
+// }
+
+// // 8.question
+// let username = "admin";
+// let password = "1234";
+
+// if (username === "admin" && password === "1234") {
+//   console.log("Login successful");
+// } else {
+//   console.log("Invalid credentials");
+// }
+// // 9.question
+// let day = 3;
+
+// switch (day) {
+//   case 1: console.log("Monday"); break;
+//   case 2: console.log("Tuesday"); break;
+//   case 3: console.log("Wednesday"); break;
+//   case 4: console.log("Thursday"); break;
+//   case 5: console.log("Friday"); break;
+//   case 6: console.log("Saturday"); break;
+//   case 7: console.log("Sunday"); break;
+//   default: console.log("Invalid day");
+// }
+// 10.question
+
+let num = 15;
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("Divisible by both");
 } else {
-  console.log("Negative number");
+  console.log("Not divisible by both");
 }
-// 2.question
-let number = 7;
-
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
-}
-// 3.question
-let age = 17;
-
-if (age >= 18) {
-  console.log("Eligible to vote");
-} else {
-  console.log("Not eligible to vote");
-}
-// 4.question
-let marks = 82;
-
-if (marks >= 90) {
-  console.log("Grade A");
-} else if (marks >= 75) {
-  console.log("Grade B");
-} else if (marks >= 50) {
-  console.log("Grade C");
-} else {
-  console.log("Grade F");
-}
-// 5.question
-let year = 2024;
-
-if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-  console.log("Leap year");
-} else {
-  console.log("Not a leap year");
-}
-// 6.question
-let x = -3;
-
-let result = (x < 0) ? "Negative number" : "Not negative";
-console.log(result);
